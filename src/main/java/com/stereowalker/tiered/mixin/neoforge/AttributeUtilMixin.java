7013233656ac0bcc06862008dbfab105e23341be
Stereowalker <stereowalker.mc@gmail.com>
2025-04-17 16:07:39 +0100

    Fix attributes not being colored

---
+++ src/main/java/com/stereowalker/tiered/mixin/neoforge/AttributeUtilMixin.java
@@ -0,0 +1,39 @@
+package com.stereowalker.tiered.mixin.neoforge;
+
+import java.util.function.Consumer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.google.common.collect.Multimap;
+import com.stereowalker.tiered.Reforged;
+import com.stereowalker.tiered.api.PotentialAttribute;
+
+import net.minecraft.core.Holder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.neoforged.neoforge.common.util.AttributeTooltipContext;
+import net.neoforged.neoforge.common.util.AttributeUtil;
+
+@Mixin(AttributeUtil.class)
+public abstract class AttributeUtilMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "net/minecraft/world/entity/ai/attributes/Attribute.toComponent(Lnet/minecraft/world/entity/ai/attributes/AttributeModifier;Lnet/minecraft/world/item/TooltipFlag;)Lnet/minecraft/network/chat/MutableComponent;"), method = "applyTextFor")
+    private static MutableComponent getTextFormatting(Attribute translatableText, AttributeModifier modif, TooltipFlag flag, ItemStack stack, Consumer<Component> tooltip, Multimap<Holder<Attribute>, AttributeModifier> modifierMap, AttributeTooltipContext ctx) {
+    	boolean isTiered = modif.id().toString().contains("tiered_");
+        if(Reforged.hasModifier(stack) && isTiered) {
+            ResourceLocation tier = stack.get(Reforged.ComponentsRegistry.MODIFIER);
+            PotentialAttribute attribute = Reforged.TIER_DATA.getTiers().get(tier);
+
+            return translatableText.toComponent(modif, flag).setStyle(attribute.getStyle());
+        } else {
+            return translatableText.toComponent(modif, flag);
+        }
+    }
+}
