839948dc75bc00f634aee17deaff5c1a7815d6fb
Stereowalker <stereowalker.mc@gmail.com>
2026-05-11 23:12:59 +0100

    Changed namspace to reforged

--- src/main/java/com/stereowalker/tiered/compat/CuriosCompat.java
+++ src/main/java/com/stereowalker/tiered/compat/CuriosCompat.java
@@ -3,7 +3,7 @@ package com.stereowalker.tiered.compat;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import com.stereowalker.tiered.Reforged;
+import com.stereowalker.reforged.Reforged;
 
 
 public class CuriosCompat {
