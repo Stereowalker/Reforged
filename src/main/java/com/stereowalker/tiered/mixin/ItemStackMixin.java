6f8cd17ee74065b32a44270cf62bfac8ad4b5499
Stereowalker <stereowalker.mc@gmail.com>
2026-05-11 23:12:59 +0100

    Changed namspace to reforged

--- src/main/java/com/stereowalker/tiered/mixin/ItemStackMixin.java
+++ src/main/java/com/stereowalker/tiered/mixin/ItemStackMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.stereowalker.tiered.Reforged;
+import com.stereowalker.reforged.Reforged;
 
 import net.minecraft.core.Holder;
 import net.minecraft.world.entity.EquipmentSlotGroup;
