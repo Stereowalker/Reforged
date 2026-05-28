0ffb03676e6171de0d035c3f125fd830cae59f44
Stereowalker <stereowalker.mc@gmail.com>
2026-05-28 15:57:12 +0100

    Prevented loading an attribute in a tier if it doesn't exist

--- src/main/java/com/stereowalker/tiered/data/TierDataLoader.java
+++ src/main/java/com/stereowalker/tiered/data/TierDataLoader.java
@@ -1,6 +1,8 @@
 package com.stereowalker.tiered.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +13,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.stereowalker.tiered.api.AttributeTemplate;
 import com.stereowalker.tiered.api.PotentialAttribute;
 import com.stereowalker.tiered.gson.AccessoryGroupDeserializer;
 import com.stereowalker.tiered.gson.AccessorySlotDeserializer;
@@ -67,7 +70,16 @@ public class TierDataLoader extends SimpleJsonResourceReloadListener implements 
             }
         }
 
-        itemAttributes = readItemAttributes;
+    	for (var attr : readItemAttributes.entrySet()) {
+    		List<AttributeTemplate> exists = new ArrayList<>();
+    		for (var att : attr.getValue().getUnfilteredAttributes()) {
+    			if (att.attributeExists(attr.getKey().toString())) exists.add(att);
+    		}
+    		attr.getValue().getAttributes().clear();
+    		attr.getValue().getAttributes().addAll(exists);
+    	}
+    	itemAttributes.clear();
+        itemAttributes.putAll(readItemAttributes);
         LOGGER.info(LOADED_RECIPES_MESSAGE, readItemAttributes.size());
     }
 
