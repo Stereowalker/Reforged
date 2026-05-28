79c6f3773838ff52e70ea55673d08a1201164947
Stereowalker <stereowalker.mc@gmail.com>
2026-05-28 15:57:12 +0100

    Prevented loading an attribute in a tier if it doesn't exist

--- src/main/java/com/stereowalker/tiered/api/AttributeTemplate.java
+++ src/main/java/com/stereowalker/tiered/api/AttributeTemplate.java
@@ -243,4 +243,13 @@ public class AttributeTemplate {
             actions.accept(key/*.get()*/, cloneModifier);
         }
     }
+
+    public boolean attributeExists(String keyChecked) {
+    	Attribute key = RegistryHelper.getAttribute(VersionHelper.AttributeHelper.backportAttribute(attributeTypeID));
+        if(key == null/* || key.isEmpty()*/) {
+    		Reforged.LOGGER.warn(String.format("%s was referenced as an attribute type in %s, but it does not exist!", attributeTypeID, keyChecked));
+    		return false;
+    	}
+    	return true;
+    }
 }
